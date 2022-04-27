4f7b092b7c7baedbcd4d67192245bb60fdfb1b11
Fathullah Harun <sololeveller@gmail.com>
2022-04-27 15:05:22 +0800

    iteration 2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import data from "./data";
 import List from "./List";
 function App() {
 	const [people, setPeople] = useState(data);
+	const [data2, setData2] = useState(true);
 
 	return (
 		<main>
 			<section className="container">
 				<h3>{people.length} birthdays today</h3>
 				<List people={people} />
-				<button onClick={() => setPeople([])}>Clear all</button>
-				<button onClick={() => setPeople([people])}>Show All</button>
+				{data2 && (
+					<button
+						onClick={() => {
+							setPeople([]);
+							setData2(false);
+						}}
+					>
+						Clear all
+					</button>
+				)}
+				{!data2 && (
+					<button
+						onClick={() => {
+							setPeople(data);
+							setData2(true);
+						}}
+					>
+						Show All
+					</button>
+				)}
 			</section>
 		</main>
 	);
